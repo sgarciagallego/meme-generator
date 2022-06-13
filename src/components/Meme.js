c2545e8eaf6b118703742ea6fb01a9ddf62736b3
sgarciagallego <gallegogarciasergio@gmail.com>
2022-06-13 10:19:44 +0100

    v.0.2.0 | [Adding Randomiser on button click] - from the data pulled from imgflip

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import memesData from '../data/memesData';
 
 const Meme = () => {
+  let url;
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    
+    console.log(url);
+  }
+
   return (
     <main>
-      <form className='form'>
+      <p>{url}</p>
+      <div className='form'>
         <input 
           type='text'
           placeholder='top text'
@@ -14,8 +26,16 @@ const Meme = () => {
           placeholder='bottom text'       
           className='form--input'
         />
-        <button className='form--button'>Get a new image 🌠</button>
-      </form>
+        <button 
+          className='form--button'
+          onClick={getMemeImage}
+        >
+          Get a new image 🌠
+        </button>
+      </div>
+      <div className='meme--container'>
+        <img src='' alt='a generate meme' id='meme--img' />
+      </div>
     </main>
   )
 }
